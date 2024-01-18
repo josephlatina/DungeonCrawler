ff35cfe885df45b6fe25ab3a1b857f0dfebf96f4
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2024-01-18 11:25:48 -0700

    fix displaced attributes

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -8,19 +8,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// movement speed of character
-[SerializeField] private float movementSpeed;
-// measured in damage per second
-[SerializeField] private float attackSpeed;
-// amount of hearts character inflicts to other (full heart or half heart)
-[SerializeField] private float strength;
-// amount of heart a character has
-[SerializeField] private float healthPoints;
-[SerializeField] private enum enemyStatus { };
-
 
 public class EnemyController : MonoBehaviour
 {
+    // movement speed of character
+    [SerializeField] private float movementSpeed;
+    // measured in damage per second
+    [SerializeField] private float attackSpeed;
+    // amount of hearts character inflicts to other (full heart or half heart)
+    [SerializeField] private float strength;
+    // amount of heart a character has
+    [SerializeField] private float healthPoints;
+    // state of enemy 
+    [SerializeField] private enum enemyStatus { };
 
     // Start is called before the first frame update
     void Start()
