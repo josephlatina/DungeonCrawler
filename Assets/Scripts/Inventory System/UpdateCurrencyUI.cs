a34448e3efb441d91ab886959f7a04d79b622e46
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2024-03-06 01:42:27 -0700

    player currency UI

--- Assets/Scripts/Inventory System/UpdateCurrencyUI.cs
+++ Assets/Scripts/Inventory System/UpdateCurrencyUI.cs
@@ -8,14 +8,10 @@ public class UpdateCurrencyUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI currencyText;
 
     public PlayerStats player;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void Update()
     {
-        // currencyText.text = player.CurrenCurrency;
+        currencyText.text = $"{player.CurrentCurrency}";
     }
 }
