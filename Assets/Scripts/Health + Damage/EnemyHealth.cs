3909917af4224d7f609dc6531acd43dcaef2e49d
Joseph Latina <latinaj2@mymacewan.ca>
2024-03-09 19:35:44 -0700

    enabled currency item to spawn after enemy is defeated

--- Assets/Scripts/Health + Damage/EnemyHealth.cs
+++ Assets/Scripts/Health + Damage/EnemyHealth.cs
@@ -6,11 +6,13 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private EnemyController enemyController;
     public float currentHealthPoints;
+    private ConsumableItemController consumableItemController;
 
     void Awake()
     {
         enemyController = GetComponent<EnemyController>();
         currentHealthPoints = enemyController.GetHealthPoints();
+        consumableItemController = GameResources.Instance.consumablePrefab.GetComponent<ConsumableItemController>();
     }
 
     public void ChangeHealth(float healthChange)
@@ -19,7 +21,36 @@ public class EnemyHealth : MonoBehaviour
 
         if (currentHealthPoints <= 0)
         {
+            Transform childTransform = transform.Find("Body");
+            Vector3 enemyPosition = childTransform.position;
             Destroy(gameObject);
+            GameManager.Instance.SpawnItem(enemyPosition);
+            // // Instantiate the item
+            // GameObject lootItemGameObject = Instantiate(GameResources.Instance.chestItemPrefab, enemyPosition, Quaternion.identity);
+            // lootItemGameObject.tag = "interactableObject";
+            // ChestItem lootItem = lootItemGameObject.GetComponent<ChestItem>();
+            // Debug.Log(childTransform);
+            // Debug.Log(enemyPosition);
+            // // Instantiate the health potion
+            // ConsumableItemController itemController = lootItemGameObject.AddComponent<ConsumableItemController>();
+            // itemController.priceView = consumableItemController.priceView;
+            // itemController.priceText = consumableItemController.priceText;
+            // itemController.item = GameResources.Instance.healthPotionSO;
+            // itemController.sprite = consumableItemController.sprite;
+            // lootItemGameObject.AddComponent<CircleCollider2D>();
+
+            // Debug.Log("hello3");
+            // lootItem.Initialize(GameResources.Instance.healthPotionIcon, enemyPosition, Color.yellow);
         }
+    }
+
+    /// <summary>
+    /// Materialize the chest item
+    /// </summary>
+    private IEnumerator MaterializeItem(Color materializeColor, SpriteRenderer spriteRenderer, MaterializeEffect materializeEffect)
+    {
+        SpriteRenderer[] spriteRendererArray = new SpriteRenderer[] { spriteRenderer };
+        Debug.Log("hello4");
+        yield return StartCoroutine(materializeEffect.MaterializeRoutine(GameResources.Instance.materializeShader, materializeColor, 1f, spriteRendererArray, GameResources.Instance.litMaterial));
     }
 }
