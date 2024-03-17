72b5a8ac055b682aa70001f1ef0f4a64dfe8a90a
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2024-03-17 17:11:58 -0600

    set up boss controller
    + add placeholder asset
    + follow target player until given distance radius
    + set up animation states

---
+++ Assets/Scripts/Enemy/ParasiticInfection/ParasiticInfectionController.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+
+public class ParasiticInfectionController : EnemyController
+{
+    public Transform target;
+    private bool isIdle = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+        Debug.Log(EnemyStateMachine.CurrentState);
+
+        if (health.currentHealthPoints <= 0)
+        {
+            anim.SetBool("isDead", true);
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+
+    void Move()
+    {
+        MoveDirection();
+
+        if (rb.velocity != Vector2.zero)
+        {
+            anim.SetBool("isWalking", true);
+            EnemyStateMachine.TransitionTo(EnemyStateMachine.moveState);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            EnemyStateMachine.TransitionTo(EnemyStateMachine.idleState);
+        }
+    }
+
+    void MoveDirection()
+    {
+        if (!isIdle)
+        {
+            // Calculate the direction from current position to the target
+            Vector2 direction = ((Vector2)target.position - rb.position).normalized;
+
+            // Move the Rigidbody2D towards the target using velocity
+            rb.velocity = direction * currentMovementSpeed;
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            EnemyStateMachine.TransitionTo(EnemyStateMachine.idleState);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform == target)
+        {
+            isIdle = true;
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform == target)
+        {
+            isIdle = false;
+        }
+    }
+}
